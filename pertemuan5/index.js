8971270c26367b5f56088f07cf759018d0128efe
ARRARIAKU2m <yerry_kecak@yahoo.com>
2023-07-12 20:41:06 +0700

    Pertemuan 5

---
+++ pertemuan5/index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // Parse JSON requests
+
+let musics = [
+    {
+        id: 1,
+        title: "Cinta Luar Biasa",
+        artist: "Andmesh Kamaleng",
+    },
+    {
+        id: 2,
+        title: "Kasih Slow",
+        artist: "Jaga Orang Pu Jodoh",
+    },
+    {
+        id: 3,
+        title: "Pura Pura Lupa",
+        artist: "Mahen",
+    }
+];
+
+// Get all music
+app.get('/musics', (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.send(JSON.stringify(musics));
+});
+
+// Get music by id
+app.get('/musics/:id', (req, res) => {
+    const id = req.params.id;
+    const music = musics.find(music => music.id == id);
+    if (music) {
+        res.json(music);
+    } else {
+        res.status(404).json({ message: 'Music not found' });
+    }
+});
+
+// Create music
+app.post('/musics', (req, res) => {
+    const music = req.body;
+    music.id = musics.length + 1;
+    musics.push(music);
+    res.status(201).json({
+        info: "success",
+        message: "Data berhasil ditambahkan",
+        data: music,
+    });
+});
+
+// Update music
+app.put('/musics/:id', (req, res) => {
+    const id = req.params.id;
+    const music = req.body;
+    const index = musics.findIndex(music => music.id == id);
+    if (index !== -1) {
+        musics[index] = { ...music, id: parseInt(id) };
+        res.json({
+            info: "success",
+            message: "Data berhasil diubah",
+            data: musics[index],
+        });
+    } else {
+        res.status(404).json({ message: 'Music not found' });
+    }
+});
+
+// Delete music
+app.delete('/musics/:id', (req, res) => {
+    const id = req.params.id;
+    const index = musics.findIndex(music => music.id == id);
+    if (index !== -1) {
+        musics.splice(index, 1);
+        res.json({
+            info: "success",
+            message: "Data berhasil dihapus",
+        });
+    } else {
+        res.status(404).json({ message: 'Music not found' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
